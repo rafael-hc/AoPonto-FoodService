13f67ef3429d45ff154dccbd98646ec0ee4c3910
Rafael Henrique Carneiro <rafael.h@outlook.com>
2026-03-08 21:21:42 -0300

    Initial commit

---
+++ apps/frontend-web/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="@ao-ponto-food-service/frontend-web" />
+    </div>
+  );
+}
+
+export default App;
