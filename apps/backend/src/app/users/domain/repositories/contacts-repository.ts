219e483f3416d7120080064a91840517d5ec5033
Rafael Henrique Carneiro <rafael.h@outlook.com>
2026-03-12 05:16:03 -0300

    feat(backend-users): add repository methods and missing error classes

--- apps/backend/src/app/users/domain/repositories/contacts-repository.ts
+++ apps/backend/src/app/users/domain/repositories/contacts-repository.ts
@@ -5,4 +5,5 @@ export abstract class ContactsRepository {
   abstract findByDocument(document: string): Promise<Contact | null>;
   abstract create(contact: Contact): Promise<void>;
   abstract save(contact: Contact): Promise<void>;
+  abstract delete(contact: Contact): Promise<void>;
 }
